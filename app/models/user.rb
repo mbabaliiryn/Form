2c4ed52829344a62653c00e4f7673cf4830021e8
mbabali iryn <mbabaliiryn@gmail.com>
2020-05-11 16:44:25 +0300

    add register and update form

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :username, presence: true, length: {maximum: 20}, uniqueness: {case_sensetiveness: false}
+    validates :password, presence: true, length: {maximum: 18, minimum: 6}
+    validates :email, presence: true, uniqueness: {case_sensetiveness: true}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  
+  root "users#index"
+  get "users/new"
+  resources :users, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
