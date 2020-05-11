ae5acaa7b60b56c38474d2008699c6c18edcc9de
Patrick Nyatindo <nyatindopatrick@gmail.com>
2020-05-11 17:07:46 +0300

    edit home route, readme and fix lint errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,29 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    def create
-        @user = User.new(user_params)
+  def edit
+    @user = User.find(params[:id])
+  end
 
-        if @user.save
-          redirect_to new_user_path
-        else
-          render :new
-        end
-    
-    end
+  def update
+    @user = User.update(user_params)
+  end
 
-    def edit
-        @user = User.find(params[:id])
-    
-    end
+  private
 
-    def update
-        @user = User.update(user_params)
-    
-    end
-
-    private
-     def user_params
-        params.require(:user).permit(:username, :email, :password)
-     end
-
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  
-  root "users#index"
-  get "users/new"
+  root "users#new"
   resources :users, only: [:new, :create, :edit, :update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
